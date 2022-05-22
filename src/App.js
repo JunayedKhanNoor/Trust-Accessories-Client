5196fa87dfded887db670384bf4a53692544f0cc
163002029.junayed <junayednoor@gmail.com>
2022-05-22 08:54:54 +0600

    Navbar create

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import "./App.css";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./Pages/Shared/Navbar";
+import Home from "./Pages/Home/Home";
+import Login from "./Pages/Login/Login";
+import SignUp from "./Pages/Login/SignUp";
+import Blogs from "./Pages/Blogs/Blogs";
 
 function App() {
   return (
     <div>
-      <button class="btn">Button</button>
-      <button class="btn btn-primary">Button</button>
-      <button class="btn btn-secondary">Button</button>
-      <button class="btn btn-accent">Button</button>
-      <button class="btn btn-info">Button</button>
-      <button class="btn btn-ghost">Button</button>
+     <Navbar></Navbar> 
+     <Routes>
+       <Route path="/" element={<Home/>}/>
+       <Route path="blogs" element={<Blogs/>}/>
+       <Route path="login" element={<Login/>}/>
+       <Route path="signup" element={<SignUp/>}/>
+     </Routes>
       <ToastContainer />
     </div>
   );
