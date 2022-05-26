47d7f7c79b19ef222b34592a24ecc7d897a96492
163002029.junayed <junayednoor@gmail.com>
2022-05-26 09:40:59 +0600

    Blogs complete

--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -6,8 +6,9 @@ const Blogs = () => {
       <h1 className="text-center text-2xl md:text-4xl font-extrabold my-12">
         Question answer, all about React Js
       </h1>
+      <progress class="progress w-full bg-secondary opacity-80"></progress>
       <div
-        tabindex="0"
+        tabIndex="0"
         className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box w-full md:w-[50%] mx-auto my-8"
       >
         <div className="collapse-title text-xl font-medium">
@@ -69,8 +70,7 @@ const Blogs = () => {
         className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box w-full md:w-[50%] mx-auto my-8"
       >
         <div className="collapse-title text-xl font-medium">
-          How does prototypical inheritance work?
-
+          What are the different ways to manage a state in a React application?
         </div>
         <div className="collapse-content">
           <div className="card w-full  bg-base-100 shadow-xl mx-auto my-8">
@@ -81,8 +81,7 @@ const Blogs = () => {
                 </button>
               </div>
               <h1 className="text-xl font-bold">
-                How does prototypical inheritance work?
-
+                What are the different ways to manage a state in a React application?
               </h1>
               <div className="divider"></div>
               <p>
@@ -101,7 +100,7 @@ const Blogs = () => {
                 <span className="font-bold">Global state:</span> Global state is data we manage
                 across multiple components. Global state is necessary when we want to get and update
                 data anywhere in our app, or in multiple components at least.We can Manage Global
-                State With Redux. 
+                State With Redux.
                 <span className="font-bold"> Server state</span> Data that comes from an external
                 server that must be integrated with our UI state. Server state is a simple concept,
                 but can be hard to manage alongside all of our local and global UI state. URL state
@@ -117,7 +116,6 @@ const Blogs = () => {
       >
         <div className="collapse-title text-xl font-medium">
           How does prototypical inheritance work?
-
         </div>
         <div className="collapse-content">
           <div className="card w-full  bg-base-100 shadow-xl mx-auto my-8">
@@ -127,14 +125,14 @@ const Blogs = () => {
                   <RiNumber3 className="text-xl " />
                 </button>
               </div>
-              <h1 className="text-xl font-bold">
-                How does prototypical inheritance work?
-
-              </h1>
+              <h1 className="text-xl font-bold">How does prototypical inheritance work?</h1>
               <div className="divider"></div>
               <p>
-              The Prototypal Inheritance is a feature in javascript used to add methods and properties in objects. It is a method by which an object can inherit the properties and methods of another object. Traditionally, in order to get and set the [[Prototype]] of an object, we use Object.getPrototypeOf and Object.setPrototypeOf. Nowadays, in modern language, it is being set using __proto__.
-
+                The Prototypal Inheritance is a feature in javascript used to add methods and
+                properties in objects. It is a method by which an object can inherit the properties
+                and methods of another object. Traditionally, in order to get and set the
+                [[Prototype]] of an object, we use Object.getPrototypeOf and Object.setPrototypeOf.
+                Nowadays, in modern language, it is being set using __proto__.
               </p>
             </div>
           </div>
@@ -145,8 +143,7 @@ const Blogs = () => {
         className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box w-full md:w-[50%] mx-auto my-8"
       >
         <div className="collapse-title text-xl font-medium">
-        Why you do not set the state directly in React?
-
+          Why you do not set the state directly in React?
         </div>
         <div className="collapse-content">
           <div className="card w-full  bg-base-100 shadow-xl mx-auto my-8">
@@ -156,14 +153,29 @@ const Blogs = () => {
                   <RiNumber4 className="text-xl " />
                 </button>
               </div>
-              <h1 className="text-xl font-bold">
-              Why you do not set the state directly in React?
-
-              </h1>
+              <h1 className="text-xl font-bold">Why you do not set the state directly in React?</h1>
               <div className="divider"></div>
+              <p className="font-bold">
+                React follows Unidirectional Data Flow. Meaning, the data flow inside react should
+                and will be expected to be in a circular path.
+              </p>
               <p>
-              The Prototypal Inheritance is a feature in javascript used to add methods and properties in objects. It is a method by which an object can inherit the properties and methods of another object. Traditionally, in order to get and set the [[Prototype]] of an object, we use Object.getPrototypeOf and Object.setPrototypeOf. Nowadays, in modern language, it is being set using __proto__.
-
+                To make React work like this, developers made React similar to functional
+                programming. The rule of thumb of functional programming is immutability. Let me
+                explain it loud and clear.
+              </p>
+              <p>
+                So, when you mutate the state directly and call setState() with an empty object. The
+                previous state will be polluted with your mutation. Due to which, the shallow
+                compare and merge of two states will be disturbed or won't happen, because you'll
+                have only one state now. This will disrupt all the React's Lifecycle Methods. As a
+                result, your app will behave abnormal or even crash. Most of the times, it won't
+                affect your app because all the apps which we use for testing this are pretty small.
+                And another downside of mutation of Objects and Arrays in JavaScript is, when you
+                assign an object or an array, you're just making a reference of that object or that
+                array. When you mutate them, all the reference to that object or that array will be
+                affected. React handles this in a intelligent way in the background and simply give
+                us an API to make it work.
               </p>
             </div>
           </div>
@@ -174,8 +186,7 @@ const Blogs = () => {
         className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box w-full md:w-[50%] mx-auto my-8"
       >
         <div className="collapse-title text-xl font-medium">
-        Why you do not set the state directly in React?
-
+          How will you implement a search to find products by name?
         </div>
         <div className="collapse-content">
           <div className="card w-full  bg-base-100 shadow-xl mx-auto my-8">
@@ -186,13 +197,12 @@ const Blogs = () => {
                 </button>
               </div>
               <h1 className="text-xl font-bold">
-              Why you do not set the state directly in React?
-
+                You have an array of products. Each product has a name, price, description, etc. How
+                will you implement a search to find products by name?
               </h1>
               <div className="divider"></div>
               <p>
-              The Prototypal Inheritance is a feature in javascript used to add methods and properties in objects. It is a method by which an object can inherit the properties and methods of another object. Traditionally, in order to get and set the [[Prototype]] of an object, we use Object.getPrototypeOf and Object.setPrototypeOf. Nowadays, in modern language, it is being set using __proto__.
-
+               The answer is very simple, I can find the products by name using filter() method. I will filter from array by name that includes the string that I give in search field.
               </p>
             </div>
           </div>
@@ -203,8 +213,7 @@ const Blogs = () => {
         className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box w-full md:w-[50%] mx-auto my-8"
       >
         <div className="collapse-title text-xl font-medium">
-        Why you do not set the state directly in React?
-
+        What is a unit test? Why should write unit tests?
         </div>
         <div className="collapse-content">
           <div className="card w-full  bg-base-100 shadow-xl mx-auto my-8">
@@ -214,15 +223,14 @@ const Blogs = () => {
                   <RiNumber6 className="text-xl " />
                 </button>
               </div>
-              <h1 className="text-xl font-bold">
-              Why you do not set the state directly in React?
-
-              </h1>
+              <h1 className="text-xl font-bold">What is a unit test? Why should write unit tests?</h1>
               <div className="divider"></div>
+              <h1  className="text-xl font-bold">What is Unit Testing?</h1>
               <p>
-              The Prototypal Inheritance is a feature in javascript used to add methods and properties in objects. It is a method by which an object can inherit the properties and methods of another object. Traditionally, in order to get and set the [[Prototype]] of an object, we use Object.getPrototypeOf and Object.setPrototypeOf. Nowadays, in modern language, it is being set using __proto__.
-
+              UNIT TESTING is a type of software testing where individual units or components of a software are tested. The purpose is to validate that each unit of the software code performs as expected. Unit Testing is done during the development (coding phase) of an application by the developers. Unit Tests isolate a section of code and verify its correctness. A unit may be an individual function, method, procedure, module, or object.
               </p>
+              <h1  className="text-xl font-bold">Why Unit Testing?</h1>
+              <p>Unit Testing is important because software developers sometimes try saving time doing minimal unit testing and this is myth because inappropriate unit testing leads to high cost Defect fixing during System Testing, Integration Testing and even Beta Testing after application is built. If proper unit testing is done in early development, then it saves time and money in the end.</p>
             </div>
           </div>
         </div>
